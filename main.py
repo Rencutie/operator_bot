d8fed737d6e4106727e80ba8fda4d701846eb6c1
Marwan <mrwnchbh@gmail.com>
2024-10-12 17:16:42 +0200

    default main, !ping

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+# Define the bot with a command prefix (e.g., ! or ?)
+intents = discord.Intents.all()  # Or use Intents.all() for more permissions
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Event when the bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}!')
+
+# respond with "pong" to a "!ping" message
+@bot.command()
+async def ping(ctx):
+    print("oh")
+    await ctx.send('pong')
+
+
+bot.run(TOKEN)
