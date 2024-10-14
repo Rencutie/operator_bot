ad53a072c57fef94f7f0eb41561f198fe3bddde3
Marwan <marwan.chbihi@etu.uca.fr>
2024-10-14 18:01:29 +0200

    added slash command

--- corgs/lvlcmd.py
+++ corgs/lvlcmd.py
@@ -27,7 +27,7 @@ class LvlCmd(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administration=True)
-    async def addExp(self, ctx, member: discord.Member, amount):
+    async def addExp(self, ctx, member: discord.Member, amount:int):
         userID = str(member.id)
         dataDict = level.loadData()
         current_level = dataDict[userID]['level']
@@ -42,20 +42,51 @@ class LvlCmd(commands.Cog):
         level.saveData(dataDict)
         await ctx.send(f"added {amount} exp to {member.mention}")
     
+    @commands.slash_command(name="addExp", description="add experiance to a user")
+    @commands.has_permissions(administration=True)
+    async def slash_addExp(self, ctx, member: discord.Member, amount:int):
+        userID = str(member.id)
+        dataDict = level.loadData()
+        current_level = dataDict[userID]['level']
+        current_exp = dataDict[userID]['exp'] + amount
+        treshold = level.xp_requirements[current_level]
+        while current_exp >= treshold :
+            current_level += 1
+            current_exp -= treshold
+            treshold = level.xp_requirements[current_level]
+        dataDict[userID]['level'] = current_level
+        dataDict[userID]['exp'] = current_exp
+        level.saveData(dataDict)
+        await ctx.send(f"added {amount} exp to {member.mention}")
+
+
     @commands.command()
     @commands.has_permissions(administration=True)
-    async def setLvl(self, ctx, member: discord.Member, lvl):
+    async def setLvl(self, ctx, member: discord.Member, lvl:int):
         userID= str(member.id)
         dataDict = level.loadData()
         initLvl = dataDict[userID]['level']
         dataDict[userID]['level'] = lvl
         dataDict[userID]['exp'] = 0
         level.saveData(dataDict)
-        await ctw.send(f"Set {member.mention} level to {lvl}\nWas {initLvl}")
+        await ctx.send(f"Set {member.mention} level to {lvl}\nWas {initLvl}")
+
+
+    @commands.slash_command(name="addExp", description="set a user's level to a given number")
+    @commands.has_permissions(administration=True)
+    async def slash_setLvl(self, ctx, member: discord.Member, lvl:int):
+        userID= str(member.id)
+        dataDict = level.loadData()
+        initLvl = dataDict[userID]['level']
+        dataDict[userID]['level'] = lvl
+        dataDict[userID]['exp'] = 0
+        level.saveData(dataDict)
+        await ctx.send(f"Set {member.mention} level to {lvl}\nWas {initLvl}")
     
+
     @commands.command()
     @commands.has_permissions(administration=True)
-    async def rmexp(self, ctx, member: discord.Member, amount):
+    async def rmExp(self, ctx, member: discord.Member, amount:int):
         userID = str(member.id)
         dataDict = level.loadData()
         current_lvl = dataDict[userID]['level']
@@ -73,5 +104,29 @@ class LvlCmd(commands.Cog):
         dataDict[userID]['level'] = current_lvl
         dataDict[userID]['exp'] = current_exp
         level.saveData(dataDict)
+        await ctx.respond(f"Removed experience points from {member.mention}. They are now at level {current_lvl} with {current_exp} exp.")
+
+    
+    @commands.slash_command(name="rmExp")
+    @commands.has_permissions(administration=True)
+    async def slash_rmExp(self, ctx, member:discord.Member, amount:int):
+        userID = str(member.id)
+        dataDict = level.loadData()
+        current_lvl = dataDict[userID]['level']
+        current_exp = dataDict[userID]['exp']
+        while amount > 0:
+            if current_exp < amount:
+                amount -= current_exp
+                current_exp = 0
+                if current_lvl > 1 :
+                    current_lvl -= 1
+                    current_exp = level.xp_requirements[current_lvl]
+            else:
+                current_exp -= amount
+                amount = 0
+        dataDict[userID]['level'] = current_lvl
+        dataDict[userID]['exp'] = current_exp
+        level.saveData(dataDict)
+        await ctx.respond(f"Removed experience points from {member.mention}. They are now at level {current_lvl} with {current_exp} exp.")
 
     
