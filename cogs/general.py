c1192c518b6935a2d4713204b5a2d869773a6f89
Marwan <mrwnchbh@gmail.com>
2024-10-25 22:06:49 +0200

    maybe

--- cogs/general.py
+++ cogs/general.py
@@ -13,6 +13,15 @@ class General(commands.Cog):
     @app_commands.command(name="ping", description="check if the bot is on")
     async def slash_ping(self, Interaction: discord.Interaction):
         await interaction.response.send_message('Pong!')
+    
+    @app_commands.command(name="help", description="show list of available commands with a description and usage")
+    async def slash_help(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Help", description="List of available commands", color=discord.Color.blue())
+        embed.set_thumbnail(url=self.bot.user.avatar.url if self.bot.user.avatar else None)
+        for cmd in self.bot.tree.walk_commands():
+            embed.add_field(name=f"**/{cmd.name}**", value=f"__{cmd.description}__", inline=False)
+
+        await interaction.response.send_message(embed=embed)
 
 
 
