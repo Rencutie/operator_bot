d6eb7154a54f52a838ce13e86a63d9b93210b970
Marwan <marwan.chbihi@etu.uca.fr>
2024-12-15 19:42:10 +0100

    added rule34 command

--- cogs/general.py
+++ cogs/general.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import aiohttp
+import random
+import xml.etree.ElementTree as ET
 from level import loadData, xp_requirements
 
 class General(commands.Cog):
@@ -41,6 +44,52 @@ class General(commands.Cog):
         embed.add_field(name="experience: ", value=f"{exp} / {xp_requirements[Level]}")
         embed.add_field(name="joind at : ", value=member.joined_at.strftime("%B %d, %Y"))
         await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="rule34", description="send a random rule34 image or video")
+    @commands.is_nsfw()
+    async def slash_rule34(self, interaction: discord.Interaction, tags: str):
+        base_url = "https://api.rule34.xxx/index.php"
+        params = {
+        "page": "dapi",
+        "s": "post",
+        "q": "index",
+        "tags": tags.replace(" ", "+"),  # Replace spaces with +
+        "limit": 100
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(base_url, params=params) as response:
+                if response != 200:
+                    await interaction.response.send_message("rule34 unreachable")
+                    return
+                xml_data = await response.text()
+                root = ET.fromstring(xml_data)
+                post_count = int(root.attrib.get("count", 0))
+                page_count = int(post_count/100)
+                params = {
+                "page": "dapi",
+                "s": "post",
+                "q": "index",
+                "tags": tags.replace(" ", "+"),  # Replace spaces with +
+                "limit": 100,
+                "pid": random.randint(0,page_count)
+                }
+            async with session.get(base_url, params=params) as response:
+                if response.status != 200:
+                    await interaction.response.send_message("rule34 unreachable")
+                    return
+                xml_data = await response.text()
+                root = ET.fromstring(xml_data)
+                posts = root.findall("posts")
+                if not posts:
+                    await interaction.response.send_message("no results matching these tags")
+                    return
+                random_post = random.choice(posts) 
+                image_url = random_post.attrib.get("file_url")
+
+                if image_url :
+                    await interaction.response.send_message(image_url)
+                else:
+                    await interaction.response.send_message("weird error of selected post not having a url")
 
 async def setup(bot):
     await bot.add_cog(General(bot))
